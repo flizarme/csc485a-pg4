64f115e4456b365a2219088d23acedc3de89b42e
bbeng89@gmail.com <bbeng89@gmail.com@ce55e24b-a3de-d0ca-48b5-5cf300a53554>
2011-04-18 03:59:03 +0000

    Created some skeleton code

---
+++ trunk/src/csc485/pg4/backgammon/gameFramework/Bar.java
@@ -0,0 +1,23 @@
+package csc485.pg4.backgammon.gameFramework;
+
+public class Bar implements ICheckerSpot {
+
+	@Override
+	public void addChecker() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void removeChecker() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getNumOfCheckers() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ trunk/src/csc485/pg4/backgammon/gameFramework/DieName.java
@@ -0,0 +1,6 @@
+package csc485.pg4.backgammon.gameFramework;
+
+public enum DieName 
+{
+	D1, D2
+}
