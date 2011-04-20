860c81dc708c3de77098aa57d1e1506362bce0eb
bbeng89@gmail.com <bbeng89@gmail.com@ce55e24b-a3de-d0ca-48b5-5cf300a53554>
2011-04-20 19:51:11 +0000

    Added code to Point

--- trunk/src/csc485/pg4/backgammon/gameFramework/Point.java
+++ trunk/src/csc485/pg4/backgammon/gameFramework/Point.java
@@ -1,23 +1,65 @@
 package csc485.pg4.backgammon.gameFramework;
 
-public class Point implements ICheckerSpot {
-
+public class Point implements ICheckerSpot 
+{
+	private Player playerOccupying;
+	private int numCheckers;
+	private String coords;
+	
+	//---------constructor---------
+	public Point(String coordinates)
+	{
+		coords = coordinates;
+		numCheckers = 0;
+	}
+	
+	//----------overloaded constructor-------------
+	public Point(String coordinates, Player occupant)
+	{
+		coords = coordinates;
+		playerOccupying = occupant;
+		numCheckers = 0;
+	}
+	
+	//----------------------overloaded constructor------------------------
+	public Point(String coordinates, Player occupant, int numberOfCheckers)
+	{
+		coords = coordinates;
+		playerOccupying = occupant;
+		numCheckers = numberOfCheckers;
+	}
+	
+	public String getCoordinates()
+	{
+		return coords;
+	}
+	
+	public Player getPlayerOccupying()
+	{
+		return playerOccupying;
+	}
+	
+	public void setPlayerOccupying(Player aPlayer)
+	{
+		playerOccupying = aPlayer;
+	}
+	
 	@Override
-	public void addChecker() {
-		// TODO Auto-generated method stub
-
+	public void addChecker() 
+	{
+		numCheckers++;
 	}
 
 	@Override
-	public void removeChecker() {
-		// TODO Auto-generated method stub
-
+	public void removeChecker() 
+	{
+		numCheckers--;
 	}
 
 	@Override
-	public int getNumOfCheckers() {
-		// TODO Auto-generated method stub
-		return 0;
+	public int getNumOfCheckers() 
+	{
+		return numCheckers;
 	}
 
 }
