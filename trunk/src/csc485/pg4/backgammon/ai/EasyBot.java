0d499772d53ab88f11f51b4f99a52c5868f8157c
bbeng89@gmail.com <bbeng89@gmail.com@ce55e24b-a3de-d0ca-48b5-5cf300a53554>
2011-04-20 23:09:09 +0000

    Added an interface for the AI. Changed ComputerPlayer to EasyBot and had it implement the interface.

---
+++ trunk/src/csc485/pg4/backgammon/ai/EasyBot.java
@@ -0,0 +1,36 @@
+package csc485.pg4.backgammon.ai;
+
+public class EasyBot implements IComputerPlayer 
+{
+
+	@Override
+	public void startTurn() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void findAvailableMoves() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void move() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void moveFromBar() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void bearOff() 
+	{
+		// TODO Auto-generated method stub
+	}
+
+}
