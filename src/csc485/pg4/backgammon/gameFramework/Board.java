365496bfc3af2708ef570f2c329639b68d1f85b0
bbeng89@gmail.com <bbeng89@gmail.com@ce55e24b-a3de-d0ca-48b5-5cf300a53554>
2011-04-20 20:15:08 +0000

    Added code to Board class. Removed a constructor from Point.

--- src/csc485/pg4/backgammon/gameFramework/Board.java
+++ src/csc485/pg4/backgammon/gameFramework/Board.java
@@ -1,5 +1,67 @@
 package csc485.pg4.backgammon.gameFramework;
 
-public class Board {
+public class Board 
+{
+	public Point[] points;
+	public Bar redBar;
+	public Bar blackBar;
+	private final int numPoints = 24;
+	private Player red, black;
+	
+	public Board(Player red, Player black)
+	{
+		points = new Point[numPoints];
+		this.red = red;
+		this.black = black;
+		redBar = new Bar();
+		blackBar = new Bar();
+		createBoard();
+		
+	}
+	
+	private void createBoard()
+	{
+		for(int i = 1; i <= numPoints; i++)
+		{
+			switch(i)
+			{
+			case 1:
+				points[i] = new Point(Integer.toString(i), red, 2);
+				break;
+			case 6:
+				points[i] = new Point(Integer.toString(i), black, 2);
+				break;
+			case 8:
+				points[i] = new Point(Integer.toString(i), black, 3);
+				break;
+			case 12:
+				points[i] = new Point(Integer.toString(i), red, 5);
+				break;
+			case 13:
+				points[i] = new Point(Integer.toString(i), black, 5);
+				break;
+			case 17:
+				points[i] = new Point(Integer.toString(i), red, 3);
+				break;
+			case 19:
+				points[i] = new Point(Integer.toString(i), red, 5);
+				break;
+			case 24:
+				points[i] = new Point(Integer.toString(i), black, 2);
+				break;
+			default:
+				points[i] = new Point(Integer.toString(i));
+				break;
+			}
+		}
+	}
+	
+	@Override
+	public String toString()
+	{
+		//TODO: implement board display
+		return "";
+	}
+	
 
 }
